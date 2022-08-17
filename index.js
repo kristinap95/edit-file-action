943ea1b900bbfe2e9e7c0e48733ad37ce4d43c95
Kristina Papić <kristina.papic@ecs.co.me>
2022-08-17 10:39:26 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+const main = async() => {
+    try {
+        // getInput(name of input from action.yml file)
+        const distFolder = core.getInput('dist_folder');
+        //const jsFile = core.getInput('js_file');
+        // const GitToken = core.getInput('token');
+        // const octokit = new github.getOctokit(GitToken);
+
+        console.log(distFolder)
+        
+    }
+    catch(error) {
+        core.setFailed(error.message);
+    }
+}
+
+
+main();
