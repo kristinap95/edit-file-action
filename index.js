00f143a95549697b88863b20c88ab8d87bacf9cc
Kristina Papić <kristina.papic@ecs.co.me>
2022-08-18 11:50:28 +0200

    add env file

--- index.js
+++ index.js
@@ -2,16 +2,11 @@ const core   = require('@actions/core');
 const github = require('@actions/github');
 const { Octokit } = require("@octokit/action");
 const { createActionAuth } = require("@octokit/auth-action");
-// or: import { createActionAuth } from "@octokit/auth-action";
 
 const main = async() => {
     try {
         // getInput(name of input from action.yml file)
-        const distFolder = core.getInput('dist_folder');
         const githubToken = core.getInput('token');
-        //const jsFile = core.getInput('js_file');
-        // const GitToken = core.getInput('token');
-        // const octokit = new github.getOctokit(GitToken);
 
         const auth = createActionAuth();
         const authentication = await auth();
@@ -21,8 +16,8 @@ const main = async() => {
           })
 
         const octokitAPI = await octokit.request('GET /repos/{owner}/{repo}/actions/artifacts', {
-            owner: 'kristinap95',
-            repo: 'vue-chat'
+            owner: process.env.OWNER,
+            repo:  process.env.REPOSITORY,
           })
         console.log("API results = ", octokitAPI);
     }
