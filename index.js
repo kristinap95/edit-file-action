dc09d835738b71fd23f0cff7247450be244f3872
Kristina Papić <kristina.papic@ecs.co.me>
2022-08-19 09:48:52 +0200

    testing

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ const main = async() => {
         // getInput(name of input from action.yml file)
         const githubToken = core.getInput('token');
 
-        const octokit = new Octokit({
-            auth: githubToken,
-          })
-        //console.log(githubToken)
-        const octokitAPI = await octokit.request('GET /repos/{owner}/{repo}/actions/artifacts', {
-            owner: process.env.OWNER,
-            repo:  process.env.REPOSITORY,
-          })
-        console.log("API results = ", octokitAPI);
+        console.log(githubToken)
+
+        // const octokit = new Octokit({
+        //     auth: githubToken,
+        //   })
+        // const octokitAPI = await octokit.request('GET /repos/{owner}/{repo}/actions/artifacts', {
+        //     owner: process.env.OWNER,
+        //     repo:  process.env.REPOSITORY,
+        //   })
+        // console.log("API results = ", octokitAPI);
     }
     catch(error) {
         core.setFailed(error.message);
